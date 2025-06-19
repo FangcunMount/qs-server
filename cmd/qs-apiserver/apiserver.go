9f977105ff894c1cfa84b0aafbcf6eec18c87927
yangshujie <yangshujie@fangcunyisheng.com>
2025-06-19 15:08:30 +0800

    truncate

--- cmd/qs-apiserver/apiserver.go
+++ cmd/qs-apiserver/apiserver.go
@@ -1,20 +1,7 @@
-// apiserver is the api server for iam-apiserver service.
-// it is responsible for serving the platform RESTful resource management.
 package main
 
-import (
-	"math/rand"
-	"time"
-
-	_ "go.uber.org/automaxprocs"
-
-	_ "go.uber.org/automaxprocs"
-
-	"github.com/yshujie/questionnaire-scale/internal/apiserver"
-)
+import "fmt"
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	apiserver.NewApp("qs-apiserver").Run()
+	fmt.Println("qs-apiserver will start")
 }
--- internal/pkg/middleware/doc.go
+++
@@ -1,2 +0,0 @@
-// Package middleware defines multiple gin middlewares
-package middleware
