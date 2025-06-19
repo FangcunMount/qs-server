40110bd5564a8ad9fcd660b8fd8fdacaab36fc56
yangshujie <yangshujie@fangcunyisheng.com>
2025-06-19 19:04:17 +0800

    feat 实现 version 包，管理 app 版本

---
+++ pkg/version/verflag/verflag.go
@@ -0,0 +1,97 @@
+package verflag
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	flag "github.com/spf13/pflag"
+
+	"github.com/yshujie/questionnaire-scale/pkg/version"
+)
+
+// versionValue 版本值
+type versionValue int
+
+// 定义一些常量
+const (
+	VersionFalse versionValue = 0
+	VersionTrue  versionValue = 1
+	VersionRaw   versionValue = 2
+)
+
+// strRawVersion 原始版本
+const strRawVersion string = "raw"
+
+// IsBoolFlag 是否为布尔标志
+func (v *versionValue) IsBoolFlag() bool {
+	return true
+}
+
+// Get 获取版本值
+func (v *versionValue) Get() interface{} {
+	return v
+}
+
+// Set 设置版本值
+func (v *versionValue) Set(s string) error {
+	if s == strRawVersion {
+		*v = VersionRaw
+		return nil
+	}
+	boolVal, err := strconv.ParseBool(s)
+	if boolVal {
+		*v = VersionTrue
+	} else {
+		*v = VersionFalse
+	}
+	return err
+}
+
+// String 返回版本值字符串
+func (v *versionValue) String() string {
+	if *v == VersionRaw {
+		return strRawVersion
+	}
+	return fmt.Sprintf("%v", bool(*v == VersionTrue))
+}
+
+// Type 返回标志类型
+func (v *versionValue) Type() string {
+	return "version"
+}
+
+// VersionVar 定义一个标志
+func VersionVar(p *versionValue, name string, value versionValue, usage string) {
+	*p = value
+	flag.Var(p, name, usage)
+	// "--version" will be treated as "--version=true"
+	flag.Lookup(name).NoOptDefVal = "true"
+}
+
+// Version 包装 VersionVar 函数
+func Version(name string, value versionValue, usage string) *versionValue {
+	p := new(versionValue)
+	VersionVar(p, name, value, usage)
+	return p
+}
+
+const versionFlagName = "version"
+
+var versionFlag = Version(versionFlagName, VersionFalse, "Print version information and quit.")
+
+// AddFlags 注册此包的标志到任意 FlagSets，使得它们指向全局标志
+func AddFlags(fs *flag.FlagSet) {
+	fs.AddFlag(flag.Lookup(versionFlagName))
+}
+
+// PrintAndExitIfRequested 检查 -version 标志是否被传递，如果传递，则打印版本并退出
+func PrintAndExitIfRequested() {
+	if *versionFlag == VersionRaw {
+		fmt.Printf("%#v\n", version.Get())
+		os.Exit(0)
+	} else if *versionFlag == VersionTrue {
+		fmt.Printf("%s\n", version.Get())
+		os.Exit(0)
+	}
+}
---
+++ pkg/version/version.go
@@ -0,0 +1,78 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/gosuri/uitable"
+
+	"github.com/yshujie/questionnaire-scale/pkg/json"
+)
+
+var (
+	// GitVersion 版本号
+	GitVersion = "v0.0.0-master+$Format:%h$"
+	// BuildDate 构建日期
+	BuildDate = "1970-01-01T00:00:00Z"
+	// GitCommit 提交哈希
+	GitCommit = "$Format:%H$"
+	// GitTreeState 树状态
+	GitTreeState = ""
+)
+
+// Info 版本信息
+type Info struct {
+	GitVersion   string `json:"gitVersion"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
+}
+
+// String 返回版本信息字符串
+func (info Info) String() string {
+	if s, err := info.Text(); err == nil {
+		return string(s)
+	}
+
+	return info.GitVersion
+}
+
+// ToJSON 返回版本信息JSON
+func (info Info) ToJSON() string {
+	s, _ := json.Marshal(info)
+
+	return string(s)
+}
+
+// Text 返回版本信息文本
+func (info Info) Text() ([]byte, error) {
+	table := uitable.New()
+	table.RightAlign(0)
+	table.MaxColWidth = 80
+	table.Separator = " "
+	table.AddRow("gitVersion:", info.GitVersion)
+	table.AddRow("gitCommit:", info.GitCommit)
+	table.AddRow("gitTreeState:", info.GitTreeState)
+	table.AddRow("buildDate:", info.BuildDate)
+	table.AddRow("goVersion:", info.GoVersion)
+	table.AddRow("compiler:", info.Compiler)
+	table.AddRow("platform:", info.Platform)
+
+	return table.Bytes(), nil
+}
+
+// Get 返回版本信息
+func Get() Info {
+	return Info{
+		GitVersion:   GitVersion,
+		GitCommit:    GitCommit,
+		GitTreeState: GitTreeState,
+		BuildDate:    BuildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
