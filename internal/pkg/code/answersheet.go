47e91d3012f13ae8a3c893d3d4bdf084cf4f0562
yangshujie <yangshujie@fangcunyisheng.com>
2025-07-05 09:47:09 +0800

    feat 更新错误码

---
+++ internal/pkg/code/answersheet.go
@@ -0,0 +1,10 @@
+package code
+
+// answersheet errors.
+const (
+	// ErrAnswerSheetNotFound - 404: Answer sheet not found.
+	ErrAnswerSheetNotFound int = iota + 110001
+
+	// ErrAnswerNotFound - 404: Answer not found.
+	ErrAnswerNotFound
+)
--- internal/pkg/code/apiserver.go
+++ internal/pkg/code/apiserver.go
@@ -23,36 +23,3 @@ const (
 	// ErrUserInactive - 403: User is inactive.
 	ErrUserInactive
 )
-
-// apiserver: questionnaire errors.
-const (
-	// ErrQuestionnaireNotFound - 404: Questionnaire not found.
-	ErrQuestionnaireNotFound int = iota + 110001
-
-	// ErrQuestionnaireAlreadyExists- 400: Questionnaire already exist.
-	ErrQuestionnaireAlreadyExists
-
-	// ErrQuestionnaireBasicInfoInvalid - 400: Questionnaire basic info is invalid.
-	ErrQuestionnaireBasicInfoInvalid
-
-	// ErrQuestionnaireStatusInvalid - 400: Questionnaire status is invalid.
-	ErrQuestionnaireStatusInvalid
-
-	// ErrQuestionnaireQuestionInvalid - 400: Questionnaire question is invalid.
-	ErrQuestionnaireQuestionInvalid
-
-	// ErrQuestionnaireQuestionNotFound - 404: Questionnaire question not found.
-	ErrQuestionnaireQuestionNotFound
-
-	// ErrQuestionnaireQuestionAlreadyExists - 400: Questionnaire question already exist.
-	ErrQuestionnaireQuestionAlreadyExists
-
-	// ErrQuestionnaireQuestionBasicInfoInvalid - 400: Questionnaire question basic info is invalid.
-	ErrQuestionnaireQuestionBasicInfoInvalid
-
-	// ErrQuestionnairePublished - 400: Questionnaire is published, can't edit.
-	ErrQuestionnairePublished
-
-	// ErrQuestionnaireArchived - 400: Questionnaire is archived, can't edit.
-	ErrQuestionnaireArchived
-)
