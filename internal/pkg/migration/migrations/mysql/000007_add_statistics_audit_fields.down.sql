00b7f5f0fec42bcb30ef460b227f624bdf4c903a
yshujie <yshujie@163.com>
2026-01-12 13:44:23 +0800

    fix 更新数据库更新脚本

--- internal/pkg/migration/migrations/mysql/000007_add_statistics_audit_fields.down.sql
+++ internal/pkg/migration/migrations/mysql/000007_add_statistics_audit_fields.down.sql
@@ -1,26 +1,26 @@
 ALTER TABLE `statistics_daily`
-    DROP COLUMN IF EXISTS `created_at`,
-    DROP COLUMN IF EXISTS `updated_at`,
-    DROP COLUMN IF EXISTS `deleted_at`,
-    DROP COLUMN IF EXISTS `created_by`,
-    DROP COLUMN IF EXISTS `updated_by`,
-    DROP COLUMN IF EXISTS `deleted_by`,
-    DROP COLUMN IF EXISTS `version`;
+    DROP COLUMN `created_at`,
+    DROP COLUMN `updated_at`,
+    DROP COLUMN `deleted_at`,
+    DROP COLUMN `created_by`,
+    DROP COLUMN `updated_by`,
+    DROP COLUMN `deleted_by`,
+    DROP COLUMN `version`;
 
 ALTER TABLE `statistics_accumulated`
-    DROP COLUMN IF EXISTS `created_at`,
-    DROP COLUMN IF EXISTS `updated_at`,
-    DROP COLUMN IF EXISTS `deleted_at`,
-    DROP COLUMN IF EXISTS `created_by`,
-    DROP COLUMN IF EXISTS `updated_by`,
-    DROP COLUMN IF EXISTS `deleted_by`,
-    DROP COLUMN IF EXISTS `version`;
+    DROP COLUMN `created_at`,
+    DROP COLUMN `updated_at`,
+    DROP COLUMN `deleted_at`,
+    DROP COLUMN `created_by`,
+    DROP COLUMN `updated_by`,
+    DROP COLUMN `deleted_by`,
+    DROP COLUMN `version`;
 
 ALTER TABLE `statistics_plan`
-    DROP COLUMN IF EXISTS `created_at`,
-    DROP COLUMN IF EXISTS `updated_at`,
-    DROP COLUMN IF EXISTS `deleted_at`,
-    DROP COLUMN IF EXISTS `created_by`,
-    DROP COLUMN IF EXISTS `updated_by`,
-    DROP COLUMN IF EXISTS `deleted_by`,
-    DROP COLUMN IF EXISTS `version`;
+    DROP COLUMN `created_at`,
+    DROP COLUMN `updated_at`,
+    DROP COLUMN `deleted_at`,
+    DROP COLUMN `created_by`,
+    DROP COLUMN `updated_by`,
+    DROP COLUMN `deleted_by`,
+    DROP COLUMN `version`;
