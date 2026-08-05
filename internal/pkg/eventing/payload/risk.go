c17e5adcda2795f9b0cc2dcb0d77990ee23196b3
yshujie <yshujie@163.com>
2026-08-06 03:37:08 +0800

    refactor: 删除事件风险零调用包装

--- internal/pkg/eventing/payload/risk.go
+++
@@ -1,8 +0,0 @@
-package eventpayload
-
-import "github.com/FangcunMount/qs-server/internal/pkg/eventing/outcome"
-
-// IsHighRiskCode reports whether code maps to high or severe risk.
-func IsHighRiskCode(code string) bool {
-	return eventoutcome.IsHighRiskCode(code)
-}
